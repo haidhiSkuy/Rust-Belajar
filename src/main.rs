cdebae64349b3717bfa97b029a63e3dcae03cd9f
Haidhi Angkawijana <haidhiangkawijana@gmail.com>
2025-05-03 20:14:48 +0700

    function

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,18 @@
 fn main() {
-    let x: i32 = -10; // int
-    let x: u32 = 10; // unsigned int 
+    let a : f64 = 10.0; 
+    let b : f64 = 20.0; 
 
-    let x: f32 = 3.14; // float 
+    println!("{}", segitiga(a, b));
 
-    let is_rain: bool = true; // boolean 
-    match is_rain {
-        true => println!("rain"),
-        false => println!("sunny")
-    }
-
-    // tupple
-    let tup: (i32, f32, String) = (100, 3.14, "wkwk".to_string()); 
-    println!("{} {} {}", tup.0, tup.1, tup.2); 
-
-    // Array 
-    let months = ["January", "February", "March", "April", "May", "June", "July",
-              "August", "September", "October", "November", "December"];
-    println!("{} {} {} {}", months[0], months[1], months[2], months[3]);
+    // Expression
+    let y = {
+        let x = 3;
+        x + 1
+    };
+    println!("The value of y is: {y}");
 }   
+
+
+fn segitiga(a : f64, b : f64) -> f64 { 
+    a * b / 2.0
+}
